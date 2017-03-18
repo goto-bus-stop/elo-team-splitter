90c50b34f3464b9e0002a5a9d964a59785eaef99
René Kooi <renee@kooi.me>
2017-03-18 13:59:00 +0100

    prod: Purify/minify css.

--- build.js
+++ build.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
 const browserify = require('browserify')
 const replace = require('replacestream')
+const concat = require('concat-stream')
+const purify = require('purifycss')
+const cssnano = require('cssnano')
+const after = require('after')
+
+// Promise to wait for the bundles to build before we run `purify`.s
+let onComplete
+const ready = new Promise((resolve) => {
+  onComplete = after(2, resolve)
+})
 
 browserify({ entries: 'src/app.js' })
   .transform('bubleify', {
@@ -8,10 +18,27 @@ browserify({ entries: 'src/app.js' })
     objectAssign: 'Object.assign'
   })
   .transform('sheetify/transform')
-  .plugin('css-extract', { out: 'build/bundle.css' })
+  .plugin('css-extract', { out: createCssStream })
   .bundle()
   .pipe(fs.createWriteStream('build/bundle.js'))
+  .on('close', onComplete)
 
 fs.createReadStream('index.html')
   .pipe(replace('</head>', '<link rel="stylesheet" href="bundle.css"></head>'))
   .pipe(fs.createWriteStream('build/index.html'))
+  .on('close', onComplete)
+
+function createCssStream () {
+  return concat((css) => {
+    ready.then(() => {
+      purify([
+        'build/bundle.js',
+        'build/index.html'
+      ], css, (purified) => {
+        cssnano.process(purified).then((result) => {
+          fs.writeFile('build/bundle.css', result.css)
+        })
+      })
+    })
+  })
+}
