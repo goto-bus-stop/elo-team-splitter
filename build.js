a7c4e0752b26302a3daab825426278f470562fc4
René Kooi <renee@kooi.me>
2017-03-18 13:46:05 +0100

    Add prod build

--- build.js
+++ build.js
@@ -1,8 +1,17 @@
 const fs = require('fs')
 const browserify = require('browserify')
+const replace = require('replacestream')
 
 browserify({ entries: 'src/app.js' })
-  .transform('bubleify', { jsx: 'h' })
+  .transform('bubleify', {
+    jsx: 'h',
+    objectAssign: 'Object.assign'
+  })
   .transform('sheetify/transform')
+  .plugin('css-extract', { out: 'build/bundle.css' })
   .bundle()
-  .pipe(fs.createWriteStream('bundle.js'))
+  .pipe(fs.createWriteStream('build/bundle.js'))
+
+fs.createReadStream('index.html')
+  .pipe(replace('</head>', '<link rel="stylesheet" href="bundle.css"></head>'))
+  .pipe(fs.createWriteStream('build/index.html'))
